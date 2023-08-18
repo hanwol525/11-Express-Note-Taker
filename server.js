74f3030765a2e0d7a0e9e9096e601dd29efebff0
Hannah Wolfson <128550078+hanwol525@users.noreply.github.com>
2023-08-17 19:34:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // get homepage
-app.get('*', (req, res) =>
-   res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
